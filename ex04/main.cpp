3ebf6f5a9da7eb9771dcceb72c750c143b535d2a
Aimen BENSETTI <abensett@e3r5p12.clusters.42paris.fr>
2022-06-12 21:44:41 +0200

    GO GO GO

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 04:10:35 by abensett          #+#    #+#             */
-/*   Updated: 2022/06/12 18:06:14 by abensett         ###   ########.fr       */
+/*   Updated: 2022/06/12 21:39:17 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,55 +26,60 @@ using std::getline;
 using std::size_t;
 using std::string;
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        cerr << "./replace <filename> <s1> <s2>  -> s1 will be replaced by s2  ;)" << endl;
-        return 1;
-    }
 
-    string infile = argv[1];										//name of the infile -> file to change
-    ifstream fin;
-    fin.open(infile, ios::in);										// in means input -> the file is open for reading the Internal Stream buffer supports input operations
-
-    if (!fin) 
+int main(int ac, char **av)
+{
+	if (ac != 4)
 	{
-        cerr << "Error : while opening the file " << infile << endl;
-        return 1;
-    }
+		cerr << "./replace <filename> <s1> <s2>  -> s1 will be replaced by s2  ;)" << endl;
+		return 1;
+	}
 
-    string outfile = infile + ".replace";							//name of the outfile -> newfile
-    ofstream fout;
-    fout.open(outfile, ios::out | ios::trunc);						// out means output -> open for writing, trunc -> suppress any existing content
+	string infile(av[1]);										//name of the infile -> file to change
+	ifstream fin;												//infile stream -> fichier de flux d'entree
+	fin.open(infile.c_str(), ios::in);							//in means input -> the file is open for reading the Internal Stream buffer supports input operations
+																//c_str turns string to const char *
+	if (!fin)
+	{
+		cerr << "Error : while opening the file " << infile << endl;
+		return 1;
+	}
 
-    if (!fout) {
-        cerr << "Error : while opening/creating the file " << outfile << endl;
-        return 3;
-    }
+	string outfile = infile + ".replace";							//name of the outfile -> newfile
+	ofstream fout;													//outfile stream -> fichier de flux de sortie
+	fout.open(outfile.c_str(), ios::out | ios::trunc);				//out means output -> open for writing, trunc -> suppress any existing content
+																	//c_str turns string to const char *
 
-    string tmp = "";
-    string input = "";
+	if (!fout)
+	{
+		cerr << "Error : while opening/creating the file " << outfile << endl;
+		return 3;
+	}
 
-    while (!fin.eof()) {
-        getline(fin, tmp);
-        input += tmp;
+	string tmp = "";
+	string input = "";
+	while (!fin.eof())
+	{
+		getline(fin, tmp);
+		input += tmp;
+		if (!fin.eof())
+			input += '\n';
+	}
+	fin.close();												// Closing the infile
 
-        if (!fin.eof())
-            input += '\n';
-    }
+	string s1 = av[2];
+	string s2 = av[3];
+	size_t position = input.find(s1);							//str.find(s1) return 1st occurence of s1, if not found return npos
 
-    fin.close();
-    string s1 = argv[2];
-    string s2 = argv[3];
-    size_t pos = input.find(s1);
+	while (position != string::npos)							// npos = max value of size_t = not found with find
+	{
+		input.erase(position, s1.length());						// erase len character at position
+		input.insert(position, s2);								// insert s2 at position
+		position = input.find(s1, position + s2.length());
+	}
 
-    while (pos != string::npos) {
-        input.erase(pos, s1.length());
-        input.insert(pos, s2);
-        pos = input.find(s1, pos + s2.length());
-    }
+	fout << input;
+	fout.close();												// Closing the outfile
 
-    fout << input;
-    fout.close();
-
-    return 0;
+	return 0;
 }
